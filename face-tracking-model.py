e2eb0f08e8a9dc2c0eba3b91e170c624364b9cf7
Sn0wO <felix@dahlstrom.tk>
2022-03-31 12:37:31 +0200

    Add model (with a few errors)

--- face-tracking-model.py
+++ face-tracking-model.py
@@ -1,2 +1,89 @@
+import os
 import tensorflow as tf 
-print("Tensorflow version:", tf.__version__)
+import matplotlib as plt
+print("Tensorflow version:", tf.__version__)   
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+TRAIN_DIRECTORY_LOCATION = r'C:\Users\Deltagare\Desktop\GTG-tracker\Datasets\face_dataset_train_images\train-images'
+VAL_DIRECTORY_LOCATION = r'C:\Users\Deltagare\Desktop\GTG-tracker\Datasets\face_dataset_val_images\val_images'
+TARGET_SIZE = (600,600) 
+CLASS_MODE = 'categorical' 
+
+train_generator = train_datagen.flow_from_directory(
+    TRAIN_DIRECTORY_LOCATION,
+    target_size = TARGET_SIZE,  
+    batch_size = 128,
+    class_mode = CLASS_MODE
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    VAL_DIRECTORY_LOCATION,
+    target_size = TARGET_SIZE,  
+    batch_size = 128,
+    class_mode = CLASS_MODE
+)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', 
+                           input_shape=(600, 600, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+model.summary()
+
+LOSS_FUNCTION = 'categorical_crossentropy'
+OPTIMIZER = r'adam'
+
+model.compile(
+    loss = LOSS_FUNCTION,
+    optimizer = OPTIMIZER,
+    metrics = ['accuracy']
+)
+
+NUM_EPOCHS = 10
+
+history = model.fit(
+      train_generator, 
+      epochs = NUM_EPOCHS,
+      verbose = 1,
+      validation_data = validation_generator)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.xlim([0,NUM_EPOCHS])
+plt.ylim([0.4,1.0])
+plt.show()
