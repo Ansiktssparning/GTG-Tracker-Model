d517743a5daf20719bdace88027d9bb8ff90c47a
Sn0wO <felix@dahlstrom.tk>
2022-04-01 10:41:57 +0200

    Train model to detect faces

--- face-tracking-model.py
+++ face-tracking-model.py
@@ -72,58 +72,59 @@ loss0, accuracy0 = model.evaluate(validation_dataset)
 print("initial loss: {:.2f}".format(loss0))
 print("initial accuracy: {:.2f}".format(accuracy0))
 
-EPOCHS = 0
+EPOCHS = 15
 history = model.fit(train_dataset,
                     epochs=EPOCHS,
                     validation_data=validation_dataset)
 
+model.save("models/GTG-tracker1")
+
 
 # opencv object that will detect faces for us
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades 
-                                     + 'haarcascade_frontalface_default.xml')
-video_capture = cv2.VideoCapture(0)  # webcamera
-
-if not video_capture.isOpened():
-    print("Unable to access the camera")
-else:
-    print("Access to the camera was successfully obtained")
-
-print("Streaming started")
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
-        minNeighbors=5,
-        minSize=(100, 100),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    for (x, y, w, h) in faces:
-        # for each face on the image detected by OpenCV
-        # draw a rectangle around the face
-        cv2.rectangle(frame, 
-                      (x, y), # start_point
-                      (x+w, y+h), # end_point
-                      (255, 0, 0),  # color in BGR
-                      2) # thickness in px
-        
+#face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades 
+#                                     + 'haarcascade_frontalface_default.xml')
+#video_capture = cv2.VideoCapture(0)  # webcamera
+#
+#if not video_capture.isOpened():
+#    print("Unable to access the camera")
+#else:
+#    print("Access to the camera was successfully obtained")
+#
+#print("Streaming started")
+#while True:
+#    # Capture frame-by-frame
+#    ret, frame = video_capture.read()
+#    if not ret:
+#        print("Can't receive frame (stream end?). Exiting ...")
+#        break
+#
+#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#        gray,
+#        scaleFactor=1.3,
+#        minNeighbors=5,
+#        minSize=(100, 100),
+#        flags=cv2.CASCADE_SCALE_IMAGE
+#    )
+#
+#    for (x, y, w, h) in faces:
+#        # for each face on the image detected by OpenCV
+#        # draw a rectangle around the face
+#        cv2.rectangle(frame, 
+#                      (x, y), # start_point
+#                      (x+w, y+h), # end_point
+#                      (255, 0, 0),  # color in BGR
+#                      2) # thickness in px
+#        
     # Display the resulting frame
-    cv2.imshow("Face detector - to quit press ESC", frame)
-
+#    cv2.imshow("Face detector - to quit press ESC", frame)
+#
     # Exit with ESC
-    key = cv2.waitKey(1)
-    if key % 256 == 27: # ESC code
-        break
+#    key = cv2.waitKey(1)
+#    if key % 256 == 27: # ESC code
+#        break
         
 # When everything done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
-print("Streaming ended")
+#video_capture.release()
+#cv2.destroyAllWindows()
+#print("Streaming ended")
