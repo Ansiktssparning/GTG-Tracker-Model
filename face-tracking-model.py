e1b55fc61f40612663d083117fd2600ef10947b7
Sn0wO <felix@dahlstrom.tk>
2022-04-01 08:09:53 +0200

    Change model to use VWW

--- face-tracking-model.py
+++ face-tracking-model.py
@@ -1,89 +1,129 @@
 import os
 import tensorflow as tf 
 import matplotlib as plt
+import os
+import numpy
+import cv2
 print("Tensorflow version:", tf.__version__)   
 
-train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
+TRAIN_DIRECTORY_LOCATION = r'Datasets\face_dataset_train_images'
+VAL_DIRECTORY_LOCATION = r'Datasets\face_dataset_val_images'
+IMG_SIZE=(600,600)
+BATCH_SIZE = 32
 
-validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
+train_dataset = tf.keras.utils.image_dataset_from_directory(TRAIN_DIRECTORY_LOCATION,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
 
-TRAIN_DIRECTORY_LOCATION = r'C:\Users\Deltagare\Desktop\GTG-tracker\Datasets\face_dataset_train_images\train-images'
-VAL_DIRECTORY_LOCATION = r'C:\Users\Deltagare\Desktop\GTG-tracker\Datasets\face_dataset_val_images\val_images'
-TARGET_SIZE = (600,600) 
-CLASS_MODE = 'categorical' 
+validation_dataset = tf.keras.utils.image_dataset_from_directory(VAL_DIRECTORY_LOCATION,
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE,
+                                                  image_size=IMG_SIZE)
 
-train_generator = train_datagen.flow_from_directory(
-    TRAIN_DIRECTORY_LOCATION,
-    target_size = TARGET_SIZE,  
-    batch_size = 128,
-    class_mode = CLASS_MODE
-)
+class_names = train_dataset.class_names
 
-validation_generator = validation_datagen.flow_from_directory(
-    VAL_DIRECTORY_LOCATION,
-    target_size = TARGET_SIZE,  
-    batch_size = 128,
-    class_mode = CLASS_MODE
-)
-
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu', 
-                           input_shape=(600, 600, 3)),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(3, activation='softmax')
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
 ])
+
+preprocess_input = tf.keras.applications.mobilenet.preprocess_input
+
+# Create the base model from the pre-trained model MobileNet V2
+IMG_SHAPE = IMG_SIZE + (3,)
+base_model = tf.keras.applications.MobileNet(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+base_model.trainable = False
+
+base_model.summary()
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+print(feature_batch_average.shape)
+
+prediction_layer = tf.keras.layers.Dense(1)
+prediction_batch = prediction_layer(feature_batch_average)
+print(prediction_batch.shape)
+
+inputs = tf.keras.Input(shape=(600, 600, 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
 model.summary()
 
-LOSS_FUNCTION = 'categorical_crossentropy'
-OPTIMIZER = r'adam'
+loss0, accuracy0 = model.evaluate(validation_dataset)
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
 
-model.compile(
-    loss = LOSS_FUNCTION,
-    optimizer = OPTIMIZER,
-    metrics = ['accuracy']
-)
+EPOCHS = 0
+history = model.fit(train_dataset,
+                    epochs=EPOCHS,
+                    validation_data=validation_dataset)
 
-NUM_EPOCHS = 10
 
-history = model.fit(
-      train_generator, 
-      epochs = NUM_EPOCHS,
-      verbose = 1,
-      validation_data = validation_generator)
+# opencv object that will detect faces for us
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades 
+                                     + 'haarcascade_frontalface_default.xml')
+video_capture = cv2.VideoCapture(0)  # webcamera
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.xlim([0,NUM_EPOCHS])
-plt.ylim([0.4,1.0])
-plt.show()
+if not video_capture.isOpened():
+    print("Unable to access the camera")
+else:
+    print("Access to the camera was successfully obtained")
+
+print("Streaming started")
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(100, 100),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        # for each face on the image detected by OpenCV
+        # draw a rectangle around the face
+        cv2.rectangle(frame, 
+                      (x, y), # start_point
+                      (x+w, y+h), # end_point
+                      (255, 0, 0),  # color in BGR
+                      2) # thickness in px
+        
+    # Display the resulting frame
+    cv2.imshow("Face detector - to quit press ESC", frame)
+
+    # Exit with ESC
+    key = cv2.waitKey(1)
+    if key % 256 == 27: # ESC code
+        break
+        
+# When everything done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+print("Streaming ended")
